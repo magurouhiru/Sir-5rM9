18415ce9c6fcc5b0f69c5e5c3143f4b16a983e48
magurouhiru <asdflkj169@gmail.com>
2026-03-20 20:11:25 +0000

    mod

--- sir_5rm9/ollama.py
+++ sir_5rm9/ollama.py
@@ -3,16 +3,14 @@ import logging
 from discord.ext import commands
 from ollama import AsyncClient, Image, Message
 
-from .logger import log_command
-
 logger = logging.getLogger(__name__)
 
 client = AsyncClient(
     host="http://ollama:11434", headers={"x-some-header": "some-value"}
 )
-models: set[str] = set()
 BASE_MODEL = "qwen3-vl:latest"
-DEFAULT_MODEL = "ark-analyzer"
+ANALYZE_MODEL = "ark-analyzer"
+ANALYZE_TARGET_CHANNEL_NAME = "ARK-レベル算出"
 
 
 def setup_ollama(bot: commands.Bot):
@@ -27,6 +25,8 @@ def setup_ollama(bot: commands.Bot):
         logger.info("Ollama モデルの確認!")
         resp = await client.list()
         models = {m.model for m in resp.models if m.model is not None}
+        logger.info(f"利用可能なモデル： {models}")
+        # ベースのモデルがなければpull
         if BASE_MODEL in models:
             logger.info(f"モデル {BASE_MODEL} が見つかりました。")
         else:
@@ -36,60 +36,61 @@ def setup_ollama(bot: commands.Bot):
             logger.info(f"モデル {BASE_MODEL} をプルします...")
             await client.pull(model=BASE_MODEL)
             logger.info(f"モデル {BASE_MODEL} のプルが完了しました。")
-        await client.create(
-            model="ark-analyzer",
-            from_="qwen3-vl:latest",
-            system="""
-            あなたは「ARK: Survival Ascended」というゲーム画面の解析のエキスパートです。
-            画面のスクリーンショットや画面の写真からステータスを特定することが可能です。
+        # 画像解析のモデルがなければcreate
+        if ANALYZE_MODEL in models:
+            logger.info(f"モデル {ANALYZE_MODEL} が見つかりました。")
+        else:
+            logger.warning(
+                f"モデル {ANALYZE_MODEL} が見つかりませんでした。利用可能なモデル: {models}"
+            )
+            logger.info(f"モデル {ANALYZE_MODEL} を作成します...")
+            await client.create(
+                model=ANALYZE_MODEL,
+                from_=BASE_MODEL,
+                system="""
+                あなたは「ARK: Survival Ascended」というゲーム画面の解析のエキスパートです。
+                画面のスクリーンショットや画面の写真からステータスを特定することが可能です。
 
-            回答する際はすべて日本語で回答してください。
-            
-            画面の大部分は3つの要素に分けられます。
-            - 左側：自分のインベントリのアイテムの情報
-            - 中央：テイム中の生物の情報
-            - 右側：テイム中の生物のインベントリのアイテムの情報
-            中央のテイム中の生物の情報のみを解析し、他は無視してください。
+                回答する際はすべて日本語で回答してください。
+                
+                画面の大部分は3つの要素に分けられます。
+                - 左側：自分のインベントリのアイテムの情報
+                - 中央：テイム中の生物の情報
+                - 右側：テイム中の生物のインベントリのアイテムの情報
+                中央のテイム中の生物の情報のみを解析し、他は無視してください。
 
-            中央のテイム中の生物の情報から下記情報を取得してください。
-            - 名前
-            - 体力
-            - スタミナ
-            - 酸素量
-            - 食料
-            - 重量
-            - 近接攻撃力
-            - 気絶値
-            このうち、体力、スタミナ、酸素量、食料、重量、気絶値の値は現在の値/マックスの値の形式になっているので、
-            マックスの値のみを取得してください。
-            また、近接攻撃力は%で表示されているので、100で割り%なし(例:150%→1.5)にしてください。
+                中央のテイム中の生物の情報から下記情報を取得してください。
+                - 名前
+                - 体力
+                - スタミナ
+                - 酸素量
+                - 食料
+                - 重量
+                - 近接攻撃力
+                - 気絶値
+                このうち、体力、スタミナ、酸素量、食料、重量、気絶値の値は現在の値/マックスの値の形式になっているので、
+                マックスの値のみを取得してください。
+                また、近接攻撃力は%で表示されているので、100で割り%なし(例:150%→1.5)にしてください。
 
-            取得した値は下記の形式の{}に埋め込みリンクを出力してください。
-            https://magurouhiru.github.io/ASB-web/#/ASB-web/calc_level?n={名前}&h={体力}&s={スタミナ}&o={酸素量}&f={食料}&w={重量}&m={近接攻撃力}&t={気絶値}
+                取得した値は下記の形式の{}に埋め込みリンクを出力してください。
+                https://magurouhiru.github.io/ASB-web/#/ASB-web/calc_level?n={名前}&h={体力}&s={スタミナ}&o={酸素量}&f={食料}&w={重量}&m={近接攻撃力}&t={気絶値}
 
-            注意事項：
-            - 名前が読み取れない場合は空文字で、それ以外は0で出力してください。
-            """,
-        )
+                注意事項：
+                - 名前が読み取れない場合は空文字で、それ以外は0で出力してください。
+                """,
+            )
+            logger.info(f"モデル {ANALYZE_MODEL} の作成が完了しました。")
         logger.info("Ollama 準備完了!")
 
-    @bot.command()
-    @log_command
-    async def ollama_list(ctx: commands.Context):
-        resp = await client.list()
-        models = {m.model for m in resp.models if m.model is not None}
-        await ctx.send(f"Ollama models: {models}")
+    @bot.event
+    async def on_message(ctx: commands.Context):
+        # 自身のメッセージには反応しない。
+        if ctx.author == bot.user:
+            return
+        # 指定のチャンネル以外では反応しない。
+        if ctx.channel.name == ANALYZE_TARGET_CHANNEL_NAME:
+            return
 
-    @bot.command()
-    @log_command
-    async def ollama_pull(ctx: commands.Context):
-        resp = await client.pull(model=BASE_MODEL)
-        await ctx.send(f"Ollama models: {resp}")
-
-    @bot.command()
-    @log_command
-    async def ollama_chat(ctx: commands.Context, *args: str):
-        message = " ".join(args)
         images = [
             await a.read()
             for a in ctx.message.attachments
@@ -100,8 +101,8 @@ def setup_ollama(bot: commands.Bot):
         else:
             images = None
         resp = await client.chat(
-            model=DEFAULT_MODEL,
-            messages=[Message(role="user", content=message, images=images)],
+            model=ANALYZE_MODEL,
+            messages=[Message(role="user", images=images)],
             stream=False,
         )
-        await ctx.send(f"Ollama: {resp.message.content}")
+        await ctx.send(resp.message.content)
