ea27328556414ee027a8ad969de096d1e4427583
magurouhiru <73399968+magurouhiru@users.noreply.github.com>
2026-03-28 00:09:20 +0000

    fix

--- packages/bot/ocr/builder.py
+++ packages/bot/ocr/builder.py
@@ -1,20 +1,21 @@
 from google.auth.transport.requests import Request
 from google.oauth2 import id_token
 
-from ocr.easy_ocr import EasyOcrImageReader
+# from ocr.easy_ocr import EasyOcrImageReader
 from ocr.ocr_server import OcrServerImageReader
 from settings.settings import settings
 
 
 def builder():
-    if settings.with_ocr_server:
-        return create_ocr_server_reader()
-    else:
-        return create_easy_ocr_reader()
+    # if settings.with_ocr_server:
+    #     return create_ocr_server_reader()
+    # else:
+    #     return create_easy_ocr_reader()
+    return create_ocr_server_reader()
 
 
-def create_easy_ocr_reader():
-    return EasyOcrImageReader()
+# def create_easy_ocr_reader():
+#     return EasyOcrImageReader()
 
 
 def create_ocr_server_reader():
