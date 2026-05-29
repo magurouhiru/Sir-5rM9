17a2d6ec457580aadc826cb11240518146a89ece
magurouhiru <73399968+magurouhiru@users.noreply.github.com>
2026-05-29 15:33:38 +0000

    fix

--- packages/dev/dev_ark_analyzer.py
+++ packages/dev/dev_ark_analyzer.py
@@ -8,9 +8,9 @@ from PIL import Image
 
 # image_path = "sample.png"  # tb:テイム前
 # image_path = "sample2.png"  # bl:ブリ
-# image_path = "sample3.jpg"  # tano:テイム後かつ酸素量なし
+image_path = "sample3.jpg"  # tano:テイム後かつ酸素量なし
 # image_path = "sample4.png"  # bl:ブリ
-image_path = "sample5.png"  # tbno:テイム前かつ酸素量なし
+# image_path = "sample5.png"  # tbno:テイム前かつ酸素量なし
 
 
 async def main():
